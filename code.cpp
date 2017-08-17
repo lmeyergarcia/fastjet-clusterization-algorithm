e45b407b00ed520d4a932964acfed62bca1ae9d3
lmeyergarcia <31008922+lmeyergarcia@users.noreply.github.com>
2017-08-17 17:25:37 -0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -30,21 +30,25 @@ int main(){
 			trackFile << x_1000 << " " << y_1000 << endl;
 		}
 	}
-	/*	
+	
 	vector<vector<unsigned int> > id_results;
+	ofstream real("reais.txt");
 	for(int i = 0; i < trackSegments.size(); i++){
 		for(int j = 0; j < trackSegments[i].size(); j++){
 			for (int k = 0; k < id_results.size(); k++){
 				unsigned int id1 = trackSegments[i][j].trackSegment[0].m_id; 
 				unsigned int id2 = trackSegments[i][j].trackSegment[1].m_id; //id dos hits do simpleSegment
-				status_id_1 = find(id_results[k].begin(), id_results.end(), id1);
-				status_id_2 = find(id_results[k].begin(), id_results.end(), id2);
-				if (status_id_1 != id_results.end() && status_id_2 != results.end)
-					good*/
+				status_id_1 = find(id_results[k].begin(), id_results[k].end(), id1);
+				status_id_2 = find(id_results[k].begin(), id_results[k].end(), id2);
+				if (status_id_1 != id_results.end() && status_id_2 != results.end){
+					float x_1000 = trackSegments[i][j].getX_1000();
+					float y_1000 = trackSegments[i][j].getY_1000();
+					real << x_1000 << " " << y_1000 << endl;
+				}
 			}
 		}
 	}
-			
+
 //	vector<TrackS> tracks = teste.getTracks();
 //	cout << "Rodando codigo: compareTracks(tracks)" << endl;
 //	data.compareTracks(tracks);

67091174128f93cb0ca4fd37aea7d59ff90c7135
lmeyergarcia <31008922+lmeyergarcia@users.noreply.github.com>
2017-09-03 13:49:46 -0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -50,7 +50,8 @@ int main(){
 				if (status_id_1 != id_results[k].end() && status_id_2 != id_results[k].end()){
 					float x_1000 = trackSegments[i][j].getX_1000();
 					float y_1000 = trackSegments[i][j].getY_1000();
-					real << k << " " << x_1000 << " " << y_1000 << endl;
+					real << k << " " << x_1000 << " " << y_1000 << endl; // indice | x_1000 | y_1000
+
 				}
 			}
 		}
